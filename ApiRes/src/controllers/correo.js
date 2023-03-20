d0483399aaf0bfbb0833513b969d81b2ce75eb27
Jaydy Michel Marin <jmmarin901@misena.edu.co>
2023-03-19 21:58:22 -0500

    Cambio ortográfico de correo y cambio nombres modal proveedor

--- ApiRes/src/controllers/correo.js
+++ ApiRes/src/controllers/correo.js
@@ -22,7 +22,7 @@ const sendMail = async(dato) => {
     const info = await transporter.sendMail({
         from: '"Jailer Shop" <shopj2353@gmail.com>',
         to: `${dato.correo}`,
-        subject: `Bienvenido ha Jailer Shop ${dato.nombre}`,
+        subject: `Bienvenido a Jailer Shop ${dato.nombre}`,
         html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
         <html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" style="width:100%;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0">
          <head>
