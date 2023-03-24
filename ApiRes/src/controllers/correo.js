9c7cd7f35bd56e9fde89ebd547b75fa9eb65ee10
jhon1996leyder <08jhonleider@gmail.com>
2023-03-24 06:51:36 -0500

    jhon

--- ApiRes/src/controllers/correo.js
+++ ApiRes/src/controllers/correo.js
@@ -19,10 +19,6 @@ const sendMail = async(dato) => {
     const info = await transporter.sendMail({
         from: '"Jailer Shop" <nicolasrioscastillo8@gmail.com>',
         to: `${dato.correo}`,
-<<<<<<< HEAD
-        subject: `Bienvenido ha Jailer Shop ${dato.nombre}`,
-
-=======
         subject: `Bienvenido a Jailer Shop ${dato.nombre}`,
         html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
         <html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" style="width:100%;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0">
@@ -236,7 +232,6 @@ const sendMail = async(dato) => {
           </div>
          </body>
         </html>`
->>>>>>> 5dddd9a71098d4c01f696c75da10c77cc3dba145
     })
 
     console.log("Message sent: %s", info.messageId)
