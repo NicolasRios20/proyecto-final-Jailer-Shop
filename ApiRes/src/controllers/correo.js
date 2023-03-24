9b705ae1c9084e867d2e43b9f42db57f4dace618
Jaydy Michel Marin <jmmarin901@misena.edu.co>
2023-03-24 06:31:24 -0500

    model compra

--- ApiRes/src/controllers/correo.js
+++ ApiRes/src/controllers/correo.js
@@ -19,6 +19,7 @@ const sendMail = async(dato) => {
     const info = await transporter.sendMail({
         from: '"Jailer Shop" <nicolasrioscastillo8@gmail.com>',
         to: `${dato.correo}`,
+
         subject: `Bienvenido a Jailer Shop ${dato.nombre}`,
         html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
         <html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" style="width:100%;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0">
