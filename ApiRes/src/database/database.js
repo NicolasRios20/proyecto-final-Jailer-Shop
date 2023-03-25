02a413cce4a991014b8106e2670162efb57b8350
NicolasRios20 <nicolasrioscastillo8@gmail.com>
2023-03-25 16:07:06 -0500

    cambio de 500 a 1000

--- ApiRes/src/database/database.js
+++ ApiRes/src/database/database.js
@@ -2,7 +2,7 @@ import mysql from "promise-mysql";
 import config from "./../config";
 
 const connection = mysql.createPool({
-    connectionLimit: 500,
+    connectionLimit: 1000,
     host: config.host,
     database: config.database,
     user: config.user,
